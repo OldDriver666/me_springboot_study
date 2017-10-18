0d6f1701c49ad2968864ee9163c936ffe2e59139
tan <jayi1314@163.com>
2017-10-18 16:09:06 +0800

    Spring Boot 启动加载数据 CommandLineRunner

---
+++ src/main/java/com/fise/tan/runner/MyStartupRunner1.java
@@ -0,0 +1,22 @@
+package com.fise.tan.runner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * 服务启动执行
+ *
+ * @author   单红宇(365384722)
+ * @myblog  http://blog.csdn.net/catoop/
+ * @create    2016年1月9日
+ */
+@Order(value=2)
+@Component
+public class MyStartupRunner1 implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(">>>>>>>>>>>>>>>服务启动执行，执行加载数据等操作1111111<<<<<<<<<<<<<");
+    }
+
+}
